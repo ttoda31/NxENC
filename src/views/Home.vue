9f2f84087c4c116e915d715a946cd8e8db1c51cb
toda <t.toda31@gmail.com>
2022-01-31 06:12:09 +0900

    Modify home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,31 @@
 <template>
   <v-container
     style="height: 100%; max-height: 600px"
-    class="pa-12 d-flex flex-column"
+    class="pa-12 d-flex flex-column justify-center"
   >
     <v-card
-      class="pa-3 ma-3 grey lighten-1"
+      class="pa-4 ma-3 grey darken-2"
       to="/noenc"
+      dark
     >
-      NoENC
+      <div class="text-h6">
+        &#x2728; NoENC &#x2728;
+      </div>
+      <div class="text-body-2 pt-2 pl-3">
+        Clipping tool without encoding. &#x2702;&#x2702;&#x2702;
+      </div>
     </v-card>
     <v-card
-      class="pa-3 ma-3 grey lighten-1"
+      class="pa-4 ma-3 grey darken-2"
       to="/nxenc"
+      dark
     >
-      NxENC
+      <div class="text-h6">
+        &#x2728; NxENC &#x2728;
+      </div>
+      <div class="text-body-2 pt-2 pl-3">
+        Encoding tool to make Nx speed videos. &#x23e9;&#x23e9;&#x23e9;
+      </div>
     </v-card>
   </v-container>
 </template>
